10fb72251de2b71b0f70e9207e3c4c47df99f7a0
ace <widdefinesme@gmail.com>
2024-01-05 11:52:21 +0900

    메인에서 브랜치A를 merge / conflict 발생으로 병합 과정을 거침

--- routes/users.js
+++ routes/users.js
@@ -11,6 +11,8 @@ router.get('/', function(req, res, next) {
 
   console.log('메인에서 브랜치B를 pull 한 경우 (rebase) 기반')
   console.log('branch A update 01')
+
+  console.log('메인에서 브랜치A를 merge / conflict 발생으로 병합 과정을 거침')
 });
 
 module.exports = router;
