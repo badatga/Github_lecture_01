3b1122587c69abb26dd9e803dc037696bd8df1ce
ace <widdefinesme@gmail.com>
2024-01-05 14:37:54 +0900

    브랜치 A 두번째 신규내용

--- routes/users.js
+++ routes/users.js
@@ -6,6 +6,7 @@ router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 
   console.log('브랜치 A에서 모든 내용을 다 지우고 신규로 작성')
+  console.log('브랜치 A 두번째 신규내용')
 });
 
 module.exports = router;
