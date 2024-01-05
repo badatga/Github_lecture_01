e7c2136bff2811833f7fb7fb976389156b8030d4
ace <widdefinesme@gmail.com>
2024-01-05 12:08:01 +0900

    브랜치 A에서 모든 내용을 다 지우고 신규로 작성

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,7 @@ var router = express.Router();
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 
-  console.log('main commit 01')
-
-  console.log('branch B update 01 ')
-
-  console.log('메인에서 브랜치B를 pull 한 경우 (rebase) 기반')
-  console.log('branch A update 01')
-
-  console.log('메인에서 브랜치A를 merge / conflict 발생으로 병합 과정을 거침')
-
-  console.log('브랜치A에서 메인을 머지한 상태에 해당 로그를 추가')
+  console.log('브랜치 A에서 모든 내용을 다 지우고 신규로 작성')
 });
 
 module.exports = router;
